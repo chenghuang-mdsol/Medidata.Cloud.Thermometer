acd6d3646aa414433ef446b7b185a9b296e1929f
jshao <jshao@mdsol.com>
2015-08-14 16:08:52 -0400

    Add unit tests for JsonResponseMiddleware

---
+++ Medidata.Cloud.Thermometer.UnitTests/TestHelpers/FakeRootMiddelware.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Microsoft.Owin;
+
+namespace Medidata.Cloud.Thermometer.UnitTests.TestHelpers
+{
+    public class FakeRootMiddelware : OwinMiddleware
+    {
+        public FakeRootMiddelware() : base(null)
+        {
+        }
+
+        public override Task Invoke(IOwinContext context)
+        {
+            return Task.FromResult(context);
+        }
+    }
+}
